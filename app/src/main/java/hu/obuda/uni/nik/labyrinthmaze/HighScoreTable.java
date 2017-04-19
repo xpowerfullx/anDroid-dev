fb80a1fe3a9963faaf689be8050e382830388070
ktomix02 <konyetomi@gmail.com>
2017-04-19 21:51:10 +0200

    listview binding

--- app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/HighScoreTable.java
+++ app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/HighScoreTable.java
@@ -4,12 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import hu.obuda.uni.nik.labyrinthmaze.database.DBHandler;
+
 public class HighScoreTable extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score_table);
+        ListView list= (ListView) findViewById(R.id.highscorelist);
+        DBHandler dbh= new DBHandler(this);
+
+        Cursor dbCursor = dbHandler.loadUsers();
+        while (!dbCursor.isAfterLast()) {
+            {
+
+                list.a
+            }}
 
 
     }
