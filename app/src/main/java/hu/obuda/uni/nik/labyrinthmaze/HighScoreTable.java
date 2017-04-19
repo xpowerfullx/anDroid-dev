7a21863c9508e7f6d8ad696c70cce64354c795e9
xpowerfullx <xpowerfulx@gmail.com>
2017-04-19 21:59:51 +0200

    fix hifhScoreTable code

--- app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/HighScoreTable.java
+++ app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/HighScoreTable.java
@@ -1,26 +1,38 @@
 package hu.obuda.uni.nik.labyrinthmaze;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import hu.obuda.uni.nik.labyrinthmaze.database.DBHandler;
 
 public class HighScoreTable extends AppCompatActivity {
 
+    DBHandler dbHandler= new DBHandler(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score_table);
-        ListView list= (ListView) findViewById(R.id.highscorelist);
-        DBHandler dbh= new DBHandler(this);
+        TextView result= (TextView) findViewById(R.id.resultTextView);
 
+        StringBuilder sb = new StringBuilder();
         Cursor dbCursor = dbHandler.loadUsers();
         while (!dbCursor.isAfterLast()) {
-            {
 
-                list.a
-            }}
+            long id = dbCursor.getLong(0);
+            //Log.d("DB","id "+ id);
+            String idd = dbCursor.getString(dbCursor.getColumnIndex("id"));
+            String name = dbCursor.getString(dbCursor.getColumnIndex("name"));
+            String time = dbCursor.getString(dbCursor.getColumnIndex("time"));
+            sb.append("RANK: " +idd+" ").append("   NAME: "+name+" ").append("TIME: "+time+" ").append("\n");
+
+            dbCursor.moveToNext();
+        }
+        result.setText(sb.toString());
+
 
 
     }
--- app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/MainActivity.java
+++ app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         HighScoreTableButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-
                 // Perform action on click
             }
         });
