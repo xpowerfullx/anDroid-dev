b71c21b13149f95bcf1adb948d861f33c4ec32c2
ktomix02 <konyetomi@gmail.com>
2017-04-19 20:36:49 +0200

    teszt

---
+++ app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/HighScoreTable.java
@@ -0,0 +1,16 @@
+package hu.obuda.uni.nik.labyrinthmaze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class HighScoreTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score_table);
+
+
+    }
+}
--- app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/MainActivity.java
+++ app/src/main/java/hu/obuda/uni/nik/labyrinthmaze/MainActivity.java
@@ -1,7 +1,8 @@
 package hu.obuda.uni.nik.labyrinthmaze;
-
+import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int csodavaltozo = 5;
+
+
+
+        final Button PlayButton = (Button) findViewById(R.id.playbutton);
+        PlayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                // Perform action on click
+            }
+        });
+
+
+        final Button HighScoreTableButton = (Button) findViewById(R.id.highscorebutton);
+        HighScoreTableButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                // Perform action on click
+            }
+        });
+
+        final Button ExitButton = (Button) findViewById(R.id.exitbutton);
+        HighScoreTableButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                // Perform action on click
+            }
+        });
 
     }
 }
